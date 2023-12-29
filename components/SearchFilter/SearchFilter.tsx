28a3d19593868e265e35b436c58e6bcd7cd9be09
michaellarrubis <michaellarrubis@gmail.com>
2023-12-29 17:34:27 +0800

    build: env file

--- components/SearchFilter/SearchFilter.tsx
+++ components/SearchFilter/SearchFilter.tsx
@@ -7,7 +7,7 @@ const SearchFilter:FC<ISearchFilterProps> = ({ onChangeKeyword, keywordValue }: 
   }
 
   return (
-    <form>   
+    <form className="w-full md:w-6/12 mx-auto">   
       <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
       <div className="relative">
         <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
@@ -18,7 +18,7 @@ const SearchFilter:FC<ISearchFilterProps> = ({ onChangeKeyword, keywordValue }: 
         <input 
           type="search" 
           id="default-search" 
-          className="text-[18px] block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:transparent" 
+          className="text-[18px] w-full block p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:transparent" 
           placeholder="Search..."
           value={keywordValue}
           onChange={event => handleOnChange(event)}
